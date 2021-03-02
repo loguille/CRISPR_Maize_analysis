1aa520e1dacc31e8bedd69aa51f18217107f2a50
Loïc Guille <loic.guille@etu.univ-lyon1.fr>
2021-03-02 10:45:35 +0100

    add explanation

--- lib/transform_tag.py
+++ lib/transform_tag.py
@@ -3,8 +3,7 @@
 import sys 
 import re
 
-#fileInput = sys.argv[1]
-#fileOutput = sys.argv[2]
+#Transform tag to have a new fasta file that contain in the name of sequence the count of the sequence and the corrected count of the sequence 
 
 def write_file(input_file,output_file):
     with open(input_file ,'r') as inputF, open(output_file, 'w') as output :
@@ -17,4 +16,4 @@ def write_file(input_file,output_file):
                 output.write('>'+str(count)+'_count_'+str(element_line[3])+'_countCorrected_'+str(element_line[2])+'\n')
                 output.write(element_line[0]+'\n')
 
-#write_file(fileInput,fileOutput)
+
